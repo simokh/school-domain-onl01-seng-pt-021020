b496b262af07022ec1f93960c03ed2e6de9b197a
Simo Khouili <m.simokhouili@gmail.com>
2020-03-01 14:34:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :roster
+
+  def initialize(school)
+    @school= school
+    @roster= {}
+  end
+
+  def add_student(name,grade)
+    @name= name
+    @grade= grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+  end
+
+  binding.pry
+
+
+
+
+
+
+
+
+
+
+end
